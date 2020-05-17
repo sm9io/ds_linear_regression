ce93b2b19b6b462a4f93e1ae72c28f74115f47f1
Subhendu Mahapatro <the.subhendum@gmail.com>
2020-05-17 19:05:31 +0530

    second commit

---
+++ stratification_variance.R
@@ -0,0 +1,45 @@
+library(dslabs)
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+galton_heights <- GaltonFamilies %>%
+  filter(childNum == 1 & gender == "male") %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+conditional_avg <- galton_heights %>% filter(round(father) == 72) %>%
+  summarise(avg_son = mean(son)) %>%
+  .$avg_son
+conditional_avg
+galton_heights %>% mutate(father_strata = factor(round(father))) %>%
+  ggplot(aes(father_strata, son)) +
+  geom_boxplot() +
+  geom_point()
+galton_heights %>% mutate(father = round(father)) %>%
+  group_by(father) %>%
+  summarise(son_conditional_avg = mean(son)) %>%
+  ggplot(aes(father, son_conditional_avg)) +
+  geom_point()
+r1 <- galton_heights %>% summarise(r = cor(father, son)) %>% .$r
+galton_heights %>%
+  mutate(father = round(father)) %>%
+  group_by(father) %>%
+  summarise(son = mean(son)) %>%
+  mutate(z_father = scale(father), z_son = scale(son)) %>%
+  ggplot(aes(z_father, z_son)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = r1)
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r2 <- cor(galton_heights$father, galton_heights$son)
+m <- r2 * mu_y / mu_x
+b <- mu_y - m * mu_x
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b, slope = m)
+galton_heights %>%
+  ggplot(aes(scale(father), scale(son))) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = 0, slope = r2)
