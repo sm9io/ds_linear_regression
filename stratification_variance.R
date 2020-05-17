133b647c1202785a6d46010b700693e03d3541cc
Subhendu Mahapatro <the.subhendum@gmail.com>
2020-05-17 22:55:50 +0530

    fourth commit

--- stratification_variance.R
+++ stratification_variance.R
@@ -14,11 +14,13 @@ galton_heights %>% mutate(father_strata = factor(round(father))) %>%
   ggplot(aes(father_strata, son)) +
   geom_boxplot() +
   geom_point()
+ggsave("plots/father_son_strata_hist.jpg")
 galton_heights %>% mutate(father = round(father)) %>%
   group_by(father) %>%
   summarise(son_conditional_avg = mean(son)) %>%
   ggplot(aes(father, son_conditional_avg)) +
   geom_point()
+ggsave("plots/father_son_strata_scatter_mean.jpg")
 r1 <- galton_heights %>% summarise(r = cor(father, son)) %>% .$r
 galton_heights %>%
   mutate(father = round(father)) %>%
@@ -28,19 +30,35 @@ galton_heights %>%
   ggplot(aes(z_father, z_son)) +
   geom_point() +
   geom_abline(intercept = 0, slope = r1)
+ggsave("plots/father_son_strata_scatter_mean_line.jpg")
 mu_x <- mean(galton_heights$father)
 mu_y <- mean(galton_heights$son)
 s_x <- sd(galton_heights$father)
 s_y <- sd(galton_heights$son)
 r2 <- cor(galton_heights$father, galton_heights$son)
-m <- r2 * mu_y / mu_x
-b <- mu_y - m * mu_x
+m1 <- r2 * mu_y / mu_x
+b1 <- mu_y - m * mu_x
+m2 <- r2 * mu_x / mu_y
+b2 <- mu_x - m * mu_y
 galton_heights %>%
   ggplot(aes(father, son)) +
   geom_point(alpha = 0.5) +
-  geom_abline(intercept = b, slope = m)
+  geom_abline(intercept = b1, slope = m1)
+ggsave("plots/father_son_scatter_line.jpg")
+galton_heights %>%
+  ggplot(aes(son, father)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b2, slope = m2)
+ggsave("plots/father_son_scatter_line_2.jpg")
 galton_heights %>%
   ggplot(aes(scale(father), scale(son))) +
   geom_point(alpha = 0.5) +
   geom_abline(intercept = 0, slope = r2)
-
+ggsave("plots/father_son_scaled_scatter_line.jpg")
+galton_heights %>%
+  mutate(z_father = round((father - mean(father)) / sd(father))) %>%
+  filter(z_father %in% -2:2) %>%
+  ggplot() +
+  stat_qq(aes(sample = son)) +
+  facet_wrap(~z_father)
+ggsave("plots/father_son_strata_qq.jpg")
