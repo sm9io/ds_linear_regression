5d5248eb4ba74bb104d8ba795867110aa2213767
Subhendu Mahapatro <the.subhendum@gmail.com>
2020-06-29 03:50:03 +0530

    confounding

---
+++ confounding.R
@@ -0,0 +1,115 @@
+# generate the Monte Carlo simulation
+N <- 25
+g <- 1000000
+sim_data <- tibble(group = rep(1:g, each = N), x = rnorm(N * g), y = rnorm(N * g))
+
+# calculate correlation between X,Y for each group
+res <- sim_data %>% 
+  group_by(group) %>% 
+  summarize(r = cor(x, y)) %>% 
+  arrange(desc(r))
+res
+
+# plot points from the group with maximum correlation
+sim_data %>% filter(group == res$group[which.max(res$r)]) %>%
+  ggplot(aes(x, y)) +
+  geom_point() + 
+  geom_smooth(method = "lm")
+ggsave("plots/false_cor_scatter.jpg")
+
+# histogram of correlation in Monte Carlo simulations
+res %>% ggplot(aes(x=r)) + geom_histogram(binwidth = 0.1, color = "black")
+ggsave("plots/false_cor_hist_all.jpg")
+
+# linear regression on group with maximum correlation
+library(broom)
+sim_data %>% 
+  filter(group == res$group[which.max(res$r)]) %>%
+  do(tidy(lm(y ~ x, data = .)))
+
+# simulate independent X, Y and standardize all except entry 23
+set.seed(1985)
+x <- rnorm(100,100,1)
+y <- rnorm(100,84,1)
+x[-23] <- scale(x[-23])
+y[-23] <- scale(y[-23])
+
+# plot shows the outlier
+qplot(x, y, alpha = 0.5)
+ggsave("plots/false_cor_scatter_outlier.jpg")
+
+# outlier makes it appear there is correlation
+cor(x,y)
+cor(x[-23], y[-23])
+
+# use rank instead
+qplot(rank(x), rank(y))
+cor(rank(x), rank(y))
+ggsave("plots/false_cor_scatter_outlier_rank.jpg")
+
+# Spearman correlation with cor function
+cor(x, y, method = "spearman")
+
+# cause and effect reversal using son heights to predict father heights
+library(HistData)
+data("GaltonFamilies")
+GaltonFamilies %>%
+  filter(childNum == 1 & gender == "male") %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight) %>% 
+  do(tidy(lm(father ~ son, data = .)))
+
+# UC-Berkeley admission data
+library(dslabs)
+data(admissions)
+admissions
+
+# percent men and women accepted
+admissions %>% group_by(gender) %>% 
+  summarize(percentage = 
+              round(sum(admitted*applicants)/sum(applicants),1))
+
+# test whether gender and admission are independent
+admissions %>% group_by(gender) %>% 
+  summarize(total_admitted = round(sum(admitted / 100 * applicants)), 
+            not_admitted = sum(applicants) - sum(total_admitted)) %>%
+  select(-gender) %>% 
+  do(tidy(chisq.test(.)))
+
+# percent admissions by major
+admissions %>% select(major, gender, admitted) %>%
+  spread(gender, admitted) %>%
+  mutate(women_minus_men = women - men)
+
+# plot total percent admitted to major versus percent women applicants
+admissions %>% 
+  group_by(major) %>% 
+  summarize(major_selectivity = sum(admitted * applicants) / sum(applicants),
+            percent_women_applicants = sum(applicants * (gender=="women")) /
+              sum(applicants) * 100) %>%
+  ggplot(aes(major_selectivity, percent_women_applicants, label = major)) +
+  geom_text()
+ggsave("plots/admission_major_women_percentage.jpg")
+
+# plot number of applicants admitted and not
+admissions %>%
+  mutate(yes = round(admitted/100*applicants), no = applicants - yes) %>%
+  select(-applicants, -admitted) %>%
+  gather(admission, number_of_students, -c("major", "gender")) %>%
+  ggplot(aes(gender, number_of_students, fill = admission)) +
+  geom_bar(stat = "identity", position = "stack") +
+  facet_wrap(. ~ major)
+ggsave("plots/admission_yes_no_major.jpg")
+
+admissions %>% 
+  mutate(percent_admitted = admitted * applicants/sum(applicants)) %>%
+  ggplot(aes(gender, y = percent_admitted, fill = major)) +
+  geom_bar(stat = "identity", position = "stack")
+ggsave("plots/admission_yes_major_men_woman.jpg")
+
+# condition on major and then look at differences
+admissions %>% ggplot(aes(major, admitted, col = gender, size = applicants)) + geom_point()
+ggsave("plots/admission_major_admitted_scatter_men_women.jpg")
+
+# average difference by major
+admissions %>%  group_by(gender) %>% summarize(average = mean(admitted))
