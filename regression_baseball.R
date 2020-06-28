56e8feb5e0f6badfabbd3649543b1a07c512aa3e
Subhendu Mahapatro <the.subhendum@gmail.com>
2020-06-28 21:02:40 +0530

    baseball regression

--- regression_baseball.R
+++ regression_baseball.R
@@ -1,6 +1,7 @@
 options(digits = 3)
 library(tidyverse)
 library(Lahman)
+library(dslabs)
 fit <- Teams %>%
   filter(yearID %in% 1961:2001) %>%
   mutate(BB = BB/G, HR = HR/G, R = R/G) %>%
@@ -83,3 +84,61 @@ players %>%
   geom_point() +
   scale_x_log10()
 ggsave("plots/player_salary_rhat_1998_pos.jpg")
+playerInfo <- Fielding %>%
+  group_by(playerID) %>%
+  arrange(desc(G)) %>%
+  slice(1) %>%
+  ungroup %>%
+  left_join(Master, by="playerID") %>%
+  select(playerID, nameFirst, nameLast, POS)
+ROY <- AwardsPlayers %>%
+  filter(awardID == "Rookie of the Year") %>%
+  left_join(playerInfo, by="playerID") %>%
+  rename(rookie_year = yearID) %>%
+  right_join(Batting, by="playerID") %>%
+  mutate(AVG = H/AB) %>%
+  filter(POS != "P")
+ROY <- ROY %>%
+  filter(yearID == rookie_year | yearID == rookie_year+1) %>%
+  group_by(playerID) %>%
+  mutate(rookie = ifelse(yearID == min(yearID), "rookie", "sophomore")) %>%
+  filter(n() == 2) %>%
+  ungroup %>%
+  select(playerID, rookie_year, rookie, nameFirst, nameLast, AVG)
+ROY <- ROY %>% spread(rookie, AVG) %>% arrange(desc(rookie))
+ROY
+mean(ROY$sophomore - ROY$rookie <= 0)
+two_years <- Batting %>%
+  filter(yearID %in% 2013:2014) %>%
+  group_by(playerID, yearID) %>%
+  filter(sum(AB) >= 130) %>%
+  summarize(AVG = sum(H)/sum(AB)) %>%
+  ungroup %>%
+  spread(yearID, AVG) %>%
+  filter(!is.na(`2013`) & !is.na(`2014`)) %>%
+  left_join(playerInfo, by="playerID") %>%
+  filter(POS!="P") %>%
+  select(-POS) %>%
+  arrange(desc(`2013`)) %>%
+  select(nameFirst, nameLast, `2013`, `2014`)
+two_years
+arrange(two_years, `2013`)
+qplot(`2013`, `2014`, data = two_years)
+ggsave("plots/2013_2014_batting_avg_scatter.jpg")
+falling_object <- rfalling_object()
+falling_object %>%
+  ggplot(aes(time, observed_distance)) +
+  geom_point() +
+  ylab("Distance in meters") +
+  xlab("Time in seconds")
+ggsave("plots/falling_object_distance_time.jpg")
+fit <- falling_object %>%
+  mutate(time_sq = time^2) %>%
+  lm(observed_distance~time+time_sq, data=.)
+tidy(fit)
+augment(fit) %>%
+  ggplot() +
+  geom_point(aes(time, observed_distance)) +
+  geom_line(aes(time, .fitted), col = "blue")
+ggsave("plots/falling_object_distance_time_predit.jpg")
+tidy(fit, conf.int = TRUE)
